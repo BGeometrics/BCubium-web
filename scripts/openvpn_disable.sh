752c6fecd8d65d2384a2d19f13946d851de5f5aa
Julian Simon <julian.simon@gmail.com>
2020-01-28 07:47:53 +0000

    nodejs https

---
+++ scripts/openvpn_disable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVICE=openvpn
+STATUS=$(systemctl is-active $SERVICE)
+ACTIVE=active
+
+if [ "$STATUS" == "$ACTIVE" ] ; then
+	sudo systemctl stop $SERVICE
+        sudo systemctl disable $SERVICE
+	systemctl status $SERVICE | grep Active
+else
+	echo -n "$SERVICE process is not running"
+fi
