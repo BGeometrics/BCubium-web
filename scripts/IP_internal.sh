9387eeb29e414f222243ed7f58b9069f663772fa
Julian Simon <julian.simon@gmail.com>
2020-01-29 10:01:08 +0000

    change script ip internal for wlan0

--- scripts/IP_internal.sh
+++ scripts/IP_internal.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo -n $(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+ETH0=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+if [ "$ETH0" = "" ] ; then
+   WLAN0=$(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+   echo -n $WLAN0  
+else
+   echo -n $ETH0  
+fi
+
+#echo -n $(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
