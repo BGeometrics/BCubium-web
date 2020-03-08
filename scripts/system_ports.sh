488cc1795613ace4327616d191afbe4762a1a2a6
juliansimon <julian.simon@gmail.com>
2020-03-08 19:26:48 +0000

    Add port NGINX

--- scripts/system_ports.sh
+++ scripts/system_ports.sh
@@ -2,7 +2,6 @@
 
 DIR_SCRIPTS=/var/www/html/webconfig/scripts
 
-
 BITCOIN_PORT=$(grep "^port=" /etc/bitcoin/bitcoin.conf | cut -b 6-)
 LND_PORT=$(grep "^listen=" /etc/bitcoin/lnd.conf | cut -b 16-)
 BTCRPCEXPLORER_PORT=$(grep "^BTCEXP_PORT=" /var/lib/bitcoin/btc-rpc-explorer/.env | cut -b 13-)
@@ -10,8 +9,9 @@ RTL_PORT=$(grep "port" /var/lib/bitcoin/RTL/RTL-Config.json | cut -b 12-15)
 TOR_PORT=$(grep "^SOCKSPort" /etc/tor/torrc | cut -b 11-)
 OPENVPN_PORT=$(grep "^port " /etc/openvpn/server.conf | cut -b 6-)
 WIREGUARD_PORT=$(grep "^ListenPort" /etc/wireguard/wg0.conf | cut -b 14-)
+NGINX=$(grep " listen \[::\]:" /etc/nginx/sites-available/default | cut -d "]" -f2 | cut -d ":" -f2 | awk {'print $1'} | head -n1)
 
-echo "{\"bitcoin_port\":\"$BITCOIN_PORT\", \"lnd_port\":\"$LND_PORT\", \"btcrpcexplorer_port\":\"$BTCRPCEXPLORER_PORT\", \"rtl_port\":\"$RTL_PORT\", \"tor_port\":\"$TOR_PORT\", \"wireguard_port\":\"$WIREGUARD_PORT\", \"openvpn_port\":\"$OPENVPN_PORT\"}"
+echo "{\"bitcoin_port\":\"$BITCOIN_PORT\", \"lnd_port\":\"$LND_PORT\", \"btcrpcexplorer_port\":\"$BTCRPCEXPLORER_PORT\", \"rtl_port\":\"$RTL_PORT\", \"tor_port\":\"$TOR_PORT\", \"wireguard_port\":\"$WIREGUARD_PORT\", \"openvpn_port\":\"$OPENVPN_PORT\", \"nginx_port\":\"$NGINX\"}"
 
 
 
