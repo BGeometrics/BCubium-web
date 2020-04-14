f286c0b76e5d61d99b5c4e58a2168d3d533f57e6
juliansimon <julian.simon@gmail.com>
2020-04-14 08:13:58 +0000

    Change show size SSD and not HDD

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -6,9 +6,10 @@ CURRENT=`echo $FREE_DATA | cut -f3 -d' '`
 TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
 RAM=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
 HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
+SSD=`df -lh | awk '{if ($6 == "/mnt/disk") { print $5 }}' | head -1 | cut -d'%' -f1`
 TEMP_=`cat /sys/class/thermal/thermal_zone0/temp`
 TEMP=$(echo "scale = 2; $TEMP_/1000" | bc)
 
-echo "{\"CPU\":\"$CPU\", \"RAM\":\"$RAM\", \"HDD\":\"$HDD\", \"TEMP\":\"$TEMP\"}"
+echo "{\"CPU\":\"$CPU\", \"RAM\":\"$RAM\", \"HDD\":\"$HDD\", \"TEMP\":\"$TEMP\", \"SSD\":\"$SSD\"}"
 
 
